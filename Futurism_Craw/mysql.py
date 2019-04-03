82ce0e81822f037fdae3b95b521973d115bbd309
601739 <jinxinyi@syswin.com>
2019-04-03 19:58:54 +0800

    设置查询，查询是查询昨天的所有文章的随机3篇 设置更新，当文章成功发布后，会设置该文章的published字段为1

--- Futurism_Craw/mysql.py
+++ Futurism_Craw/mysql.py
@@ -4,21 +4,26 @@ import json
 
 #存储模块
 class MySQL():
-    def __init__(self, host=MYSQL_HOST, username=MYSQL_USER, password=MYSQL_PASSWORD, port=MYSQL_PORT,
-                 database=MYSQL_DATABASE):
-        """
-        MySQL初始化
-        :param host:
-        :param username:
-        :param password:
-        :param port:
-        :param database:
-        """
+    host = MYSQL_HOST
+    username = MYSQL_USER
+    password = MYSQL_PASSWORD
+    port = MYSQL_PORT
+    database = MYSQL_DATABASE
+
+    def __init__(self):
+        pass
+
+    def get_connection(self):
         try:
-            self.db = pymysql.connect(host, username, password, database, charset='utf8', port=port)
+            self.db = pymysql.connect(self.host, self.username, self.password, self.database, charset='utf8', port=self.port)
             self.cursor = self.db.cursor()
         except pymysql.MySQLError as e:
             print(e.args)
+
+
+    def close_connection(self):
+        if self.db:
+            self.db.close()
 
     def insert(self, table, data):
         """
@@ -38,6 +43,36 @@ class MySQL():
             print('Failed')
             self.db.rollback()
 
-    def close(self):
-        if self.db:
-            self.db.close()
+
+
+    def select(self, table, columns, filter):
+
+        columns = ', '.join(columns)
+        sql_query = 'select %s from %s where %s' % (columns, table, filter)
+        print(sql_query)
+        try:
+            self.cursor.execute(sql_query)
+            print('查询出的数量：',self.cursor.rowcount)
+            results = self.cursor.fetchall()
+            return results
+        except:
+            print('查询失败')
+
+
+    def update(self,table,setter,filter):
+        sql_query = 'update %s set %s where %s' % (table,setter,filter)
+        print(sql_query)
+        try:
+            self.cursor.execute(sql_query)
+            print('success')
+            self.db.commit()
+        except:
+            print('failed')
+            self.db.rollback()
+
+
+if __name__ == '__main__':
+    mysql = MySQL()
+    mysql.get_connection()
+    mysql.update(TARGET_TABLE,'published = 1','url = ' + '\"https://futurism.com/russia-new-shotgun-wielding-drone-action/\"')
+    mysql.close_connection()
