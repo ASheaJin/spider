08ea1040e15b2c42d6cb8e1370dd7424cfc9ac83
601739 <jinxinyi@syswin.com>
2019-04-03 20:01:06 +0800

    添加发布模块的定时任务

--- Futurism_Craw/schedule.py
+++ Futurism_Craw/schedule.py
@@ -3,36 +3,47 @@ import datetime
 import time
 from Futurism_Craw.spider import *
 from Futurism_Craw.config import *
+from Futurism_Craw.publish import Publish
 
 class Schedule():
 
     def __init__(self):
         self.spider = Futurism_Spider()
+        self.publish = Publish()
 
 
-    def doSth(self):
+    def doCraw(self):
         # 把爬虫程序放在这个类里
         for x in range(GROUP_START, GROUP_END + 1):
             self.spider.run(x * OFF_SET)
 
-        time.sleep(60)
+        time.sleep(10)
+
+    def doPublish(self):
+        self.publish.main()
+        time.sleep(10)
+
 
 
     # 一般网站都是1:00点更新数据，所以每天凌晨一点启动
-    def main(self,h, m):
+    def main(self,craw_h, craw_m,publish_h,publish_m):
         while True:
             while True:
                 now = datetime.datetime.now()
                 # print(now.hour, now.minute)
-                if now.hour == h and now.minute == m:
+                #查看是否到爬取时间
+                if now.hour == craw_h and now.minute == craw_m:
                     break
+                #查看是否到发布时间，发布的是前一天前3条文章
+                if now.hour == publish_h and now.minute == publish_m:
+                    self.doPublish()
                 # 每隔60秒检测一次
                 time.sleep(60)
 
-            self.doSth()
+            self.doCraw()
 
 
 
 if __name__ == '__main__':
     schedule = Schedule()
-    schedule.main(H,M)
+    schedule.main(CARW_H,CARW_M,PUBLISH_H,PUBLISH_M)
