ebfb05a034fcca0b4770a25d2e767bf7aa4fc52b
601739 <jinxinyi@syswin.com>
2019-04-04 17:30:19 +0800

    按照小时调度爬虫和发布模块

--- Futurism_Craw/schedule.py
+++ Futurism_Craw/schedule.py
@@ -20,13 +20,13 @@ class Schedule():
         time.sleep(10)
 
     def doPublish(self):
-        self.publish.main()
+        self.publish.main_hours()
         time.sleep(10)
 
 
 
     # 一般网站都是1:00点更新数据，所以每天凌晨一点启动
-    def main(self,craw_h, craw_m,publish_h,publish_m):
+    def main_day(self,craw_h, craw_m,publish_h,publish_m):
         while True:
             while True:
                 now = datetime.datetime.now()
@@ -42,8 +42,27 @@ class Schedule():
 
             self.doCraw()
 
+        # 一般网站都是1:00点更新数据，所以每天凌晨一点启动
+
+    def main_hours(self, craw_m, publish_m):
+        while True:
+            while True:
+                now = datetime.datetime.now()
+                # print(now.hour, now.minute)
+                # 查看是否到爬取时间
+                if  now.minute == craw_m:
+                    break
+                # 查看是否到发布时间，发布的是前一天前3条文章
+                if now.minute == publish_m:
+                    self.doPublish()
+                # 每隔60秒检测一次
+                time.sleep(60)
+
+            self.doCraw()
+
 
 
 if __name__ == '__main__':
     schedule = Schedule()
-    schedule.main(CARW_H,CARW_M,PUBLISH_H,PUBLISH_M)
+    # schedule.main_day(CARW_H,CARW_M,PUBLISH_H,PUBLISH_M)
+    schedule.main_hours(CARW_M,PUBLISH_M)
