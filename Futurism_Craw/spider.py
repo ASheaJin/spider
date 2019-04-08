6a3d0620899446c93aa74d7ea159a55b4bb42298
601739 <jinxinyi@syswin.com>
2019-04-08 16:32:42 +0800

    优化访问算法，访问3次，没有反应就跳过

--- Futurism_Craw/spider.py
+++ Futurism_Craw/spider.py
@@ -1,13 +1,16 @@
+# -*- coding: utf-8 -*-
 from  urllib.parse import urlencode
 import requests
 import time
 import json
 import pymysql
-from lxml import etree
 import re
 from Futurism_Craw.config import *
 from Futurism_Craw.data_filter import Data_Filter
 from Futurism_Craw.mysql import MySQL
+from Futurism_Craw.my_logger import Logger
+
+
 
 #对https://futurism.com 网站进行文章爬取，通过ajax请求返回数据，进行数据解析，结构化
 
@@ -22,6 +25,7 @@ class Futurism_Spider():
 
     data_filter = Data_Filter()
     mysql = MySQL()
+    logger = Logger(LOGGER_NAME).getlog()
 
     latest_url = 'https://futurism.com/wp-json/wp/v2/all-posts?'
     byte_url = 'https://futurism.com/wp-json/wp/v2/posts?'
@@ -30,70 +34,91 @@ class Futurism_Spider():
         'authority': 'futurism.com',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
         'x-requested-with': 'XMLHttpRequest',
-        'referer': 'https://futurism.com'
+        'referer': 'https://futurism.com',
+        'authorization': 'Basic Z3V0ZW1iZXJnOnNQbWt5U2tzMTgx',
+        'cookie': '_ga=GA1.2.313593188.1553562842; em_cdn_uid=t%3D1553562842379%26u%3D4b7cf968aa994279b21ccc876bfa9f66; __smVID=663baa57d0c6f2a259a337deca5bee866c8553fe1e372b71173b96cbe09517d3; __gads=ID=497970e128469b51:T=1553562844:S=ALNI_Mbyvt_dLSsVi-BUBPNE17tP0VdqaQ; __qca=P0-1687767503-1553562843711; __smListBuilderShown=Tue%20Mar%2026%202019%2009:14:20%20GMT+0800%20(%E4%B8%AD%E5%9B%BD%E6%A0%87%E5%87%86%E6%97%B6%E9%97%B4); _fbp=fb.1.1554079490081.715629428; __smToken=BuS35jwAvnHhRIjyFOnsXWOI; _gid=GA1.2.818504779.1554684291'
     }
 
     headers_byte = {
         'authority': 'futurism.com',
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36',
         'x-requested-with': 'XMLHttpRequest',
-        'origin': 'https://futurism.com',
-        'referer': 'https://www.futurism.com/the-byte'
+        # 'origin': 'https://futurism.com',
+        'referer': 'https://www.futurism.com/the-byte',
+        'authorization': 'Basic Z3V0ZW1iZXJnOnNQbWt5U2tzMTgx',
+        'cookie': '_ga=GA1.2.313593188.1553562842; em_cdn_uid=t%3D1553562842379%26u%3D4b7cf968aa994279b21ccc876bfa9f66; __smVID=663baa57d0c6f2a259a337deca5bee866c8553fe1e372b71173b96cbe09517d3; __gads=ID=497970e128469b51:T=1553562844:S=ALNI_Mbyvt_dLSsVi-BUBPNE17tP0VdqaQ; __qca=P0-1687767503-1553562843711; __smListBuilderShown=Tue%20Mar%2026%202019%2009:14:20%20GMT+0800%20(%E4%B8%AD%E5%9B%BD%E6%A0%87%E5%87%86%E6%97%B6%E9%97%B4); _fbp=fb.1.1554079490081.715629428; __smToken=BuS35jwAvnHhRIjyFOnsXWOI; _gid=GA1.2.818504779.1554684291'
+
     }
 
     # exclude[] = 155687 & exclude[] = 155649 & exclude[] = 155664 & exclude[] = 155640
-    def get_latest_page(self,page):
+    def get_latest_page(self,page,current_count = 1,req_count = 3):
+
         '''
         请求url，获取latest页json数据，然后睡眠5秒
         :param page: 第几页
         :return: json数据，为12条
         '''
-        params = {
-            'page': page,
-            'per_page': '12',
-            'filter[tag__not_in][]':73328,
-            'show_on_homepage': 'true'
-        }
-        # 'filter[tag__not_in][]=91937&filter[tag__not_in][]=73328&page=1&per_page=12&show_on_homepage=true'
-        #必须要这样写
-        url = self.latest_url + urlencode(params) + '&filter%5Btag__not_in%5D%5B%5D=91937'
-        # url = self.latest_url + 'filter%5Btag__not_in%5D%5B%5D=91937&filter%5Btag__not_in%5D%5B%5D=73328&page='+page+'&per_page=12&show_on_homepage=true'
-        print(url)
-        try:
-            response = requests.get(url, headers=self.headers_text)
-            if response.status_code == 200:
-                # print(response.json()[0])
-                return response.json()
-        except requests.ConnectionError:
-            # return None
-            #这里强制必需访问成功
-            print('再次连接访问！')
-            return self.get_latest_page(page)
-        finally:
-            time.sleep(5)
+        if current_count <= req_count:
+            params = {
+                'page': page,
+                'per_page': '12',
+                'filter[tag__not_in][]':73328,
+                'show_on_homepage': 'true'
+            }
+            #必须要这样写
+            url = self.latest_url + urlencode(params) + '&filter%5Btag__not_in%5D%5B%5D=91937'
+            print('请求的url为:%s,这是请求的第%d次' % (url,current_count))
 
-    def get_byte_page(self,page):
-        params = {
-            'page': page,
-            'per_page': '12',
-            'tags[]': 91937
-        }
-        url = self.byte_url + urlencode(params)
-        # url = self.byte_url + 'tags[]=91937&page=%d&per_page=12&show_on_homepage=true' % page
-        print(url)
-        try:
-            response = requests.get(url, headers=self.headers_byte)
+            try:
+                #如果60秒没有响应，就抛出异常
+                response = requests.get(url, headers=self.headers_text,timeout = 60)
+                #如果请求成功
+                if response.status_code == 200:
+                    print('latest请求成功')
+                    return response.json()
+                else:
+                    print('latest请求失败')
+                    self.logger.error('latest请求失败，response.status_code != 200 url: %s' % url)
+                    return None
 
-            if response.status_code == 200:
-                # print(response.json()[0])
-                return response.json()
-        except requests.ConnectionError:
-            # return None
-            print('再次连接访问！')
-            return self.get_byte_page(page)
-        finally:
-            time.sleep(5)
-        # 保存获取的信息保存到mysql中
+            except requests.ConnectionError:
+                print('再次连接访问！')
+                self.logger.error('latest中requests.get()方法发生异常，再次连接访问！url: %s' % url)
+                return self.get_latest_page(page,current_count + 1,req_count)
+        else:
+            print('latest请求超过限定次数')
+            self.logger.error('latest请求失败，请求次数超过设定值:%d' % (req_count))
+            return None
+
+
+
+#
+    def get_byte_page(self,page,current_count = 1,req_count = 3):
+        if current_count <= req_count:
+            params = {
+                'page': page,
+                'per_page': '12',
+                'tags[]': 91937
+            }
+            url = self.byte_url + urlencode(params)
+            print('请求的url为:%s,这是请求的第%d次' % (url,current_count))
+
+            try:
+                response = requests.get(url, headers=self.headers_byte)
+                if response.status_code == 200:
+                    return response.json()
+                else:
+                    print('byte请求失败')
+                    self.logger.error('byte请求失败，response.status_code != 200 url: %s' % url)
+                    return None
+            except requests.ConnectionError:
+                print('再次连接访问！')
+                self.logger.error('byte中requests.get()方法发生异常，再次连接访问！url: %s' % url)
+                return self.get_byte_page(page,current_count + 1,req_count)
+        else:
+            print('byte请求超过限定次数')
+            self.logger.error('byte请求失败，请求次数超过设定值:%d' % (req_count))
+            return None
 
 
     def data_converter_save(self, records):
@@ -111,7 +136,7 @@ class Futurism_Spider():
         for item in records:
             # 动态的content大小
 
-            #这里要改变，之前为-8
+            #这里要改变，之前为-8，这里多了个pure_text
             length = len(item) - 9
 
             pure_text = item[8 + length].get('text')
@@ -134,37 +159,42 @@ class Futurism_Spider():
             }
             self.mysql.insert(TARGET_TABLE,data)
 
-        # self.mysql.close()
-
-
     def start(self,offset):
 
-        #爬取latest页的数据
-        json_latest = self.get_latest_page(offset)
+        #爬取latest页的数据,请求超过3次还没返回就返回None
+        json_latest = self.get_latest_page(offset,1,3)
 
+        #请求的时间间隔为5秒
+        time.sleep(5)
         # print(len(json_latest))
 
         # 爬取byte页的数据
-        json_byte = self.get_byte_page(offset)
+        json_byte = self.get_byte_page(offset,1,3)
 
-        #获取数据库连接
-        self.mysql.get_connection()
+        time.sleep(5)
 
-        #对爬取的latest页的数据进行清洗
-        latest_records = self.data_filter.get_futurism_infos(json_latest)
-        #对爬取的latest页的数据进行存储
-        self.data_converter_save(latest_records)
-        # print(latest_records)
+        #只要二者有一个不为None，就开启数据库连接
+        if json_byte or json_latest:
+            #优化：获取数据库连接
+            if self.mysql.get_connection():
+                #如果请求到数据
+                if json_latest:
+                    #对爬取到的latest页的数据进行清洗
+                    latest_records = self.data_filter.get_futurism_infos(json_latest)
+                    #对爬取到的latest页的数据进行存储
+                    self.data_converter_save(latest_records)
 
-        print('=========='*10)
-        # 对爬取的byte页的数据进行清洗
-        byte_records = self.data_filter.get_futurism_infos(json_byte)
-        # 对爬取的byte页的数据进行存储
-        self.data_converter_save(byte_records)
+                if json_byte:
+                    # 对爬取到的byte页的数据进行清洗
+                    byte_records = self.data_filter.get_futurism_infos(json_byte)
+                    # 对爬取到的byte页的数据进行存储
+                    self.data_converter_save(byte_records)
 
-        #关闭数据库连接
-        self.mysql.close_connection()
-        # print(byte_records)
+                #关闭数据库连接
+                self.mysql.close_connection()
+        else:
+            print('这次爬取失败~~~~~~~~~~~~~~~~，未向数据库插入数据')
+            self.logger.warning('当前时间的爬虫未能爬取到数据')
 
 
     def run(self,offset):
